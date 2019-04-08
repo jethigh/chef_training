4bc864f4b05432671e2f9d017da46aae7ffff8c4
EC2 Default User <ec2-user@ip-10-0-0-80.ec2.internal>
2019-04-08 14:50:48 +0000

    Use od tempalte in apache, version 0.2.1

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -2,11 +2,15 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-          <h2>IPADDRESS: #{node['ipaddress']}</h2>
-          <h2>HOSTNAME:  #{node['hostname']}</h2> 
-          "
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello, world!</h1>
+#          <h2>IPADDRESS: #{node['ipaddress']}</h2>
+#          <h2>HOSTNAME:  #{node['hostname']}</h2> 
+#          "
+#end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
