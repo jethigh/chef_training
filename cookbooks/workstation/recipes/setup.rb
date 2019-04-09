dd4fb0e85bc250b5c602041edd5320b9052ebb46
EC2 Default User <ec2-user@ip-10-0-0-80.ec2.internal>
2019-04-09 06:26:33 +0000

    Added tempalte in workastion cookbook, 0.2.1

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -2,12 +2,16 @@ package 'tree' do
 	action :install
 end
 
-file '/etc/motd' do
-	content "Property of Mateusz
-
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME : #{node['hostname']}
-  MEMORY   : #{node['memory']['total']}
-  CPU      : #{node['cpu']['0']['mhz']}
-"
+#file '/etc/motd' do
+#	content "Property of Mateusz
+#
+ # IPADDRESS: #{node['ipaddress']}
+#  HOSTNAME : #{node['hostname']}
+#  MEMORY   : #{node['memory']['total']}
+#  CPU      : #{node['cpu']['0']['mhz']}
+#"
+#end
+#
+template '/etc/motd' do
+  source 'motd.erb'
 end
